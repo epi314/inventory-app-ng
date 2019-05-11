dbbd3767e70a8d488a3716d719ebd1166ec3ad74
Pierre Enriquez <pierre@perxtech.com>
2019-05-11 22:47:30 +0800

    Add a material module for importing @angular/material components modules.

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule, MatTableModule, MatSortModule, MatSelectModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+  ],
+  exports: [
+  ]
+})
+export class MaterialModule { }
